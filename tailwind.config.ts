4bb649154c8821791390b0429ba8e2d7dfb9d9f4
Robert Safin <robertsafin98@gmail.com>
2023-09-18 17:37:51 +0800

    1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        jost: "Jost",
+        inter: "Inter",
+      },
+      colors: {
+        bgBlack: "rgba(0,0,0,0.95)",
+        highlight: "#0096C3",
+        overlay: "rgba(0,150,195,0.05)",
+        ghostGray: "#A9A9A9",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
